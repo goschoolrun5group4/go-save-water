6a0ee1c61d45402b0f844b8fa66f693ccd90a9f9
Ou Shiwei <shiwei.ou@protonmail.com>
2022-06-20 02:00:35 +0800

    - refactor code

--- api/v1/user/server/handler.go
+++ api/v1/user/server/handler.go
@@ -1,8 +1,11 @@
 package server
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
+	com "go-save-water/pkg/common"
+	"go-save-water/pkg/log"
 	"io/ioutil"
 	"net/http"
 
@@ -10,181 +13,255 @@ import (
 )
 
 type UsersInfo struct {
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
-	Username  string `json:"username"`
-	Email     string `json:"email"`
+	UserID         int    `json:"userID"`
+	FirstName      string `json:"firstName"`
+	LastName       string `json:"lastName"`
+	Username       string `json:"username"`
+	HashedPassword string `json:"hashedPassword,omitempty"`
+	Email          string `json:"email"`
+	Role           string `json:"role,omitempty"`
+	IsDeleted      bool   `json:"isDeleted,omitempty"`
+	CreatedDT      string `json:"createdDT,omitempty"`
+	ModifiedDT     string `json:"modifiedDT,omitempty"`
 }
 
-func allusers(w http.ResponseWriter, r *http.Request) {
-	var user UsersInfo
+func userList(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
 
-	results, err := db.Query("Select * FROM user")
-	if err != nil {
-		panic(err.Error())
-	}
-
-	var userList []UsersInfo
-
-	for results.Next() {
-		// map this type to the record in the table
-		var user UsersInfo
-		err = results.Scan(&user.FirstName, &user.LastName, &user.Username, &user.Email)
-		userList = append(userList, user)
+		results, err := db.Query("SELECT UserID, FirstName, LastName, UserName, Email, Role, IsDeleted, CreatedDT, ModifiedDT  FROM User")
 		if err != nil {
-			panic(err.Error())
+			log.Error.Println(err)
 		}
-	}
 
-	json.NewEncoder(w).Encode(user)
+		var userList []UsersInfo
+
+		for results.Next() {
+			// map this type to the record in the table
+			var user UsersInfo
+			err = results.Scan(
+				&user.UserID,
+				&user.FirstName,
+				&user.LastName,
+				&user.Username,
+				&user.Email,
+				&user.Role,
+				&user.IsDeleted,
+				&user.CreatedDT,
+				&user.ModifiedDT,
+			)
+			userList = append(userList, user)
+			if err != nil {
+				log.Error.Println(err)
+			}
+		}
+
+		json.NewEncoder(w).Encode(userList)
+	}
 }
 
 // func courseGet to scan client input courseCode and courseTitle with sql database and return course results
-func userGet(w http.ResponseWriter, r *http.Request) {
+func userGet(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
 
-	var user UsersInfo
-
-	params := mux.Vars(r)
-	userID := params["userid"]
-
-	query := fmt.Sprintf("Select * FROM user where email = '%s' ", userID)
-
-	result := db.QueryRow(query)
-
-	err := result.Scan(&user.FirstName, &user.LastName, &user.Username, &user.Email)
-
-	if err != nil {
-		fmt.Println(err)
-		w.WriteHeader(http.StatusNotFound)
-		w.Write([]byte("404 -No user found"))
-		return
-	}
-	json.NewEncoder(w).Encode(user)
-
-}
-
-func userPost(w http.ResponseWriter, r *http.Request) {
-
-	params := mux.Vars(r)
-	userID := params["userid"]
-
-	if r.Header.Get("Content-type") == "application/json" {
-		// read the string sent to the service
-		var newUser UsersInfo
-		reqBody, err := ioutil.ReadAll(r.Body)
-		if err == nil {
-			// convert JSON to object
-			json.Unmarshal(reqBody, &newUser)
-			if newUser.Username == "" {
-				w.WriteHeader(http.StatusUnprocessableEntity)
-				w.Write([]byte("422 -Please provide FirstName " + "LastName" + "Username" + "Email"))
-				return
-			}
-			var user UsersInfo
-
-			query := fmt.Sprintf("Select * FROM Username where userID = '%s' ", userID)
-
-			result := db.QueryRow(query)
-
-			err := result.Scan(&user.FirstName, &user.LastName, &user.Username, &user.Email)
-
-			if err == nil {
-				fmt.Println(err)
-				w.WriteHeader(http.StatusConflict)
-				w.Write([]byte("409 -Duplicate user info"))
-				return
-			}
-			//query = fmt.Sprintf("UPDATE user SET username ='%s' WHERE username = '%s'", newUser.first_name, newUser.last_name)
-			query = fmt.Sprintf("INSERT INTO user VALUES ('%s', '%s')", userID, newUser.first_name, newUser.last_name, newUser.username, newUser.Email)
-			_, err = db.Query(query)
-			if err != nil {
-				fmt.Println(err)
-				w.WriteHeader(http.StatusInternalServerError)
-				w.Write([]byte("500 -Server Error"))
-				return
-			}
-		} else {
-			w.WriteHeader(http.StatusUnprocessableEntity)
-			w.Write([]byte("422 -Please supply username " + "in JSON format"))
-		}
-	}
-}
-
-func userPut(w http.ResponseWriter, r *http.Request) {
-
-	params := mux.Vars(r)
-	userID := params["userid"]
-
-	var newUser UsersInfo
-	reqBody, err := ioutil.ReadAll(r.Body)
-	if err == nil {
-		json.Unmarshal(reqBody, &newUser)
-		if newUser.Username == "" {
-			w.WriteHeader(http.StatusUnprocessableEntity)
-			w.Write([]byte("422 -Please provide username " + " email " + "in JSON format"))
-			return
-		}
 		var user UsersInfo
 
-		query := fmt.Sprintf("Select * FROM username where email = '%s' ", userID)
+		params := mux.Vars(r)
+		userID := params["userid"]
 
-		result := db.QueryRow(query)
+		// Prepared Statement
+		stmt, err := db.Prepare("SELECT UserID, FirstName, LastName, UserName, Email, Role, IsDeleted FROM User WHERE UserID = ?")
+		if err != nil {
+			log.Error.Println(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("500 - Internal Server Error"))
+			return
+		}
+		result := stmt.QueryRow(userID)
+		err = result.Scan(
+			&user.UserID,
+			&user.FirstName,
+			&user.LastName,
+			&user.Username,
+			&user.Email,
+			&user.Role,
+			&user.IsDeleted,
+		)
 
-		err := result.Scan(&course.CourseCode, &course.CourseTitle)
+		if err != nil {
+			log.Error.Println(err)
+			w.WriteHeader(http.StatusNotFound)
+			w.Write([]byte("404 - No user found"))
+			return
+		}
 
-		if err == nil {
-			query = fmt.Sprintf("UPDATE user SET email  = '%s' WHERE username = '%s'", newUser.UsersInfo, userID)
-			_, err = db.Query(query)
-			if err != nil {
-				fmt.Println(err)
-				w.WriteHeader(http.StatusInternalServerError)
-				w.Write([]byte("500 -Server Error"))
-				return
+		json.NewEncoder(w).Encode(user)
+	}
+}
+
+func userPost(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		var (
+			newUser       UsersInfo
+			userNameExist bool
+		)
+
+		// TODO: Use Middleware when merged
+		if r.Header.Get("Content-type") == "application/json" {
+			// read the string sent to the service
+			reqBody, err := ioutil.ReadAll(r.Body)
+			if err == nil {
+				// convert JSON to object
+				json.Unmarshal(reqBody, &newUser)
+
+				// TODO: Validation?
+				/*if newUser.Username == "" {
+					w.WriteHeader(http.StatusUnprocessableEntity)
+					w.Write([]byte("422 -Please provide FirstName " + "LastName" + "Username" + "Email"))
+					return
+				}*/
+
+				// Check if username exist
+				err = db.QueryRow("call spUserExistByUserName(?)", newUser.Username).Scan(&userNameExist)
+				if err != nil {
+					log.Error.Println(err)
+					w.WriteHeader(http.StatusInternalServerError)
+					w.Write([]byte("500 - Internal Server Error"))
+					return
+				}
+				if userNameExist {
+					w.WriteHeader(http.StatusConflict)
+					w.Write([]byte("409 - Username Taken"))
+					return
+				}
+				//query = fmt.Sprintf("UPDATE user SET username ='%s' WHERE username = '%s'", newUser.first_name, newUser.last_name)
+				_, err = db.Query("call spUserCreate(?, ?, ?, ?, ?, ?)",
+					newUser.Username,
+					newUser.FirstName,
+					newUser.LastName,
+					newUser.Email,
+					newUser.HashedPassword,
+					newUser.Role,
+				)
+				if err != nil {
+					log.Error.Println(err)
+					w.WriteHeader(http.StatusInternalServerError)
+					w.Write([]byte("500 - Server Error"))
+					return
+				}
+				w.WriteHeader(http.StatusCreated)
 			}
 		} else {
-			query = fmt.Sprintf("INSERT INTO user VALUES ('%s', '%s')", userID, newUser.UsersInfo)
-			_, err = db.Query(query)
-			if err != nil {
-				fmt.Println(err)
-				w.WriteHeader(http.StatusInternalServerError)
-				w.Write([]byte("500 -Server Error"))
-				return
-			}
+			w.WriteHeader(http.StatusUnprocessableEntity)
+			w.Write([]byte("422 - Please supply data in JSON format"))
 		}
-	} else {
-		w.WriteHeader(http.StatusUnprocessableEntity)
-		w.Write([]byte("422 -Please supply " + "user information " + "in JSON format"))
+	}
+}
+
+func userPut(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		params := mux.Vars(r)
+		userID := params["userid"]
+
+		var (
+			userInfo  UsersInfo
+			userExist bool
+		)
+
+		// TODO: Use Middleware when merged
+		if r.Header.Get("Content-type") == "application/json" {
+			reqBody, err := ioutil.ReadAll(r.Body)
+			if err == nil {
+				json.Unmarshal(reqBody, &userInfo)
+
+				// TODO: Validation?
+				/*if newUser.Username == "" {
+					w.WriteHeader(http.StatusUnprocessableEntity)
+					w.Write([]byte("422 -Please provide username " + " email " + "in JSON format"))
+					return
+				}*/
+
+				// Check if userID exist
+				err = db.QueryRow("call spUserExistByUserID(?)", userID).Scan(&userExist)
+				if err != nil {
+					log.Error.Println(err)
+					w.WriteHeader(http.StatusInternalServerError)
+					w.Write([]byte("500 - Internal Server Error"))
+					return
+				}
+				if !userExist {
+					w.WriteHeader(http.StatusNotFound)
+					w.Write([]byte("404 - User not Found"))
+					return
+				}
+
+				_, err = db.Query("call spUserUpdate(?, ?, ?, ?, ?, ?, ?, ?)",
+					userID,
+					com.NewNullString(userInfo.FirstName),
+					com.NewNullString(userInfo.LastName),
+					com.NewNullString(userInfo.Username),
+					com.NewNullString(userInfo.HashedPassword),
+					com.NewNullString(userInfo.Email),
+					com.NewNullString(userInfo.Role),
+					userInfo.IsDeleted,
+				)
+
+				if err != nil {
+					log.Error.Println(err)
+					w.WriteHeader(http.StatusInternalServerError)
+					w.Write([]byte("500 - Internal Server Error"))
+					return
+				}
+
+				w.WriteHeader(http.StatusAccepted)
+
+			}
+		} else {
+			w.WriteHeader(http.StatusUnprocessableEntity)
+			w.Write([]byte("422 - Please supply user information in JSON format"))
+		}
 	}
 }
 
 // func userDelete to check scan client input Username, FirstName, LastName with the database, delete if matches
-func userDelete(w http.ResponseWriter, r *http.Request) {
+func userDelete(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
 
-	var user UsersInfo
+		var userExist bool
 
-	params := mux.Vars(r)
-	userID := params["userid"]
+		params := mux.Vars(r)
+		userID := params["userid"]
 
-	query := fmt.Sprintf("Select * FROM user where username = '%s' ", userID)
+		err := db.QueryRow("call spUserExistByUserID(?)", userID).Scan(&userExist)
+		if err != nil {
+			log.Error.Println(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("500 - Internal Server Error"))
+			return
+		}
+		if !userExist {
+			w.WriteHeader(http.StatusNotFound)
+			w.Write([]byte("404 - User not Found"))
+			return
+		}
 
-	result := db.QueryRow(query)
-
-	err := result.Scan(&user.Username, &user.Email)
-
-	if err != nil {
-		fmt.Println(err)
-		w.WriteHeader(http.StatusNotFound)
-		w.Write([]byte("404 -No course found"))
-		return
+		// Prepared Statement
+		stmt, err := db.Prepare("UPDATE User SET IsDeleted = true, ModifiedDT = NOW() WHERE UserID = ?")
+		if err != nil {
+			log.Error.Println(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("500 - Internal Server Error"))
+			return
+		}
+		_, err = stmt.Query(userID)
+		if err != nil {
+			fmt.Println(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("500 -Server Error"))
+			return
+		}
+		w.WriteHeader(http.StatusAccepted)
+		w.Write([]byte("202 - User deleted: " + userID))
 	}
-	query = fmt.Sprintf("DELETE FROM username WHERE email = '%s'", userID)
-	_, err = db.Query(query)
-	if err != nil {
-		fmt.Println(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("500 -Server Error"))
-		return
-	}
-	w.WriteHeader(http.StatusAccepted)
-	w.Write([]byte("202 -User deleted: " + params["userid"]))
 }
--- api/v1/user/server/server.go
+++ api/v1/user/server/server.go
@@ -1,27 +1,54 @@
 package server
 
 import (
-	com "go-save-water/pkg/common"
-	"go-save-water/pkg/log"
+	"database/sql"
 	"net/http"
 
+	com "go-save-water/pkg/common"
+	"go-save-water/pkg/log"
+
+	"github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 )
 
 func Start() {
-	if err := http.ListenAndServe(com.GetEnvVar("PORT"), handlers()); err != nil {
+
+	db := dbConnect()
+
+	if err := http.ListenAndServe(com.GetEnvVar("PORT"), handlers(db)); err != nil {
 		log.Fatal.Fatalln("ListenAndServe: ", err)
 	}
 }
 
-func handlers() http.Handler {
+func handlers(db *sql.DB) http.Handler {
 	router := mux.NewRouter()
 	api := router.PathPrefix("/api/v1").Subrouter()
 
-	api.HandleFunc("/users", allusers)
-	api.HandleFunc("/user/{userid}", userGet).Methods("GET")
-	api.HandleFunc("/user/{userid}", userPut).Methods("PUT")
-	api.HandleFunc("/user/{userid}", userPost).Methods("POST")
-	api.HandleFunc("/user/{userid}", userDelete).Methods("DELETE")
+	api.Handle("/users", userList(db))
+	api.Handle("/user", userPost(db)).Methods("POST")
+	api.Handle("/user/{userid}", userGet(db)).Methods("GET")
+	api.Handle("/user/{userid}", userPut(db)).Methods("PUT")
+	api.Handle("/user/{userid}", userDelete(db)).Methods("DELETE")
+
 	return router
+}
+
+func dbConnect() *sql.DB {
+	dbCfg := mysql.Config{
+		User:                 com.GetEnvVar("DBUSER"),
+		Passwd:               com.GetEnvVar("DBPASS"),
+		Net:                  "tcp",
+		Addr:                 com.GetEnvVar("DBHOST") + ":" + com.GetEnvVar("DBPORT"),
+		DBName:               com.GetEnvVar("DBNAME"),
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	}
+
+	db, err := sql.Open("mysql", dbCfg.FormatDSN())
+
+	if err != nil {
+		log.Fatal.Fatalln(err)
+	}
+
+	return db
 }
